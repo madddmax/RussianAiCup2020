616da5d742f54a1118e62b630016788ec100e449
madddmax <m_prokofiev@mail.ru>
2020-12-04 03:48:52 +0300

    Added new move algorithm

--- src/MyStrategy.cs
+++ src/MyStrategy.cs
@@ -10,6 +10,28 @@ namespace Aicup2020
     {
         public Action GetAction(PlayerView playerView, DebugInterface debugInterface)
         {
+            Entity?[,] map = new Entity?[80, 80];
+
+            foreach (Entity entity in playerView.Entities)
+            {
+                var properties = playerView.EntityProperties[entity.EntityType];
+                if (properties.Size > 1)
+                {
+                    for (int y = entity.Position.Y; y < entity.Position.Y + properties.Size; y++)
+                    {
+                        for (int x = entity.Position.X; x < entity.Position.X + properties.Size; x++)
+                        {
+                            map[x, y] = entity;
+                        }
+                    }
+                }
+                else
+                {
+                    map[entity.Position.X, entity.Position.Y] = entity;
+                }
+            }
+
+
             var entityActions = new Dictionary<int, EntityAction>();
 
             var entitiesToRepair = playerView.Entities
@@ -39,9 +61,14 @@ namespace Aicup2020
                     e.EntityType == EntityType.BuilderBase
                 );
 
-            foreach (var entity in playerView.Entities)
+            foreach (Entity entity in playerView.Entities)
             {
-                if(entity.PlayerId != playerView.MyId)
+                if (entity.PlayerId != null && entity.PlayerId != playerView.MyId)
+                {
+                    continue;
+                }
+
+                if (entity.PlayerId != playerView.MyId)
                 {
                     continue;
                 }
@@ -53,123 +80,205 @@ namespace Aicup2020
                 AttackAction? attackAction = null;
                 RepairAction? repairAction = null;
 
-                if (entity.EntityType == EntityType.BuilderUnit)
+                switch (entity.EntityType)
                 {
-                    double? minDistanceToRepair = entitiesToRepair.Count > 0
-                        ? entitiesToRepair.Min(e => Distance(e.Position, entity.Position))
-                        : (double?) null;
-
-                    if (minDistanceToRepair.HasValue &&
-                        minDistanceToRepair > 1.5 &&
-                        minDistanceToRepair < 4)
-                    {
-                        var entityToRepair = entitiesToRepair.First(
-                            e => Math.Abs(Distance(e.Position, entity.Position) - minDistanceToRepair.Value) < 0.001
-                        );
-                        moveAction = new MoveAction(entityToRepair.Position, true, false);
-
-                        entityActions.Add(entity.Id, new EntityAction(moveAction, null, null, null));
+                    case EntityType.Wall:
                         continue;
-                    }
 
-                    if (minDistanceToRepair.HasValue &&
-                        minDistanceToRepair < 1.5)
-                    {
-                        var entityToRepair = entitiesToRepair.First(
-                            e => Math.Abs(Distance(e.Position, entity.Position) - minDistanceToRepair.Value) < 0.001
-                        );
-                        repairAction = new RepairAction(entityToRepair.Id);
-
-                        entityActions.Add(entity.Id, new EntityAction(null, null, null, repairAction));
+                    case EntityType.House:
                         continue;
-                    }
 
-                    var myPlayer = playerView.Players[playerView.MyId - 1];
-                    var buildEntityType = turrets > houses && houses < 15 ? EntityType.House : EntityType.Turret;
-                    var buildEntity = playerView.EntityProperties[buildEntityType];
-
-                    if (myPlayer.Resource >= buildEntity.Cost)
-                    {
+                    case EntityType.BuilderBase:
+                        var entityType = properties.Build.Value.Options[0];
                         var position = new Vec2Int(
-                            entity.Position.X + properties.Size,
-                            entity.Position.Y + properties.Size - 1
+                            entity.Position.X,
+                            entity.Position.Y - 1
                         );
 
-                        if (!IsInRect(builderBase.Value.Position, position, buildEntity.Size))
+                        buildAction = new BuildAction(entityType, position);
+                        entityActions.Add(entity.Id, new EntityAction(null, buildAction, null, null));
+                        continue;
+
+                    case EntityType.BuilderUnit:
+                    {
+                        int leftUpEval = 0;
+                        for (int y = entity.Position.Y; y >= entity.Position.Y - 10 && y >= 0; y--)
                         {
-                            buildAction = new BuildAction(buildEntityType, position);
+                            for (int x = entity.Position.X; x <= entity.Position.X + 10 && x < 80; x++)
+                            {
+                                var entityOnMap = map[x, y];
+                                if (entityOnMap == null)
+                                {
+                                    continue;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.Resource)
+                                {
+                                    var distance = GetDistance(entity.Position, x, y);
+                                    leftUpEval += entityOnMap.Value.Health / distance;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.BuilderUnit &&
+                                    entityOnMap.Value.PlayerId == playerView.MyId)
+                                {
+                                    leftUpEval -= entityOnMap.Value.Health;
+                                }
+                            }
                         }
+
+                        int rightUpEval = 0;
+                        for (int y = entity.Position.Y; y >= entity.Position.Y - 10 && y >= 0; y--)
+                        {
+                            for (int x = entity.Position.X; x >= entity.Position.X - 10 && x >= 0; x--)
+                            {
+                                var entityOnMap = map[x, y];
+                                if (entityOnMap == null)
+                                {
+                                    continue;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.Resource)
+                                {
+                                    var distance = GetDistance(entity.Position, x, y);
+                                    rightUpEval += entityOnMap.Value.Health / distance;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.BuilderUnit &&
+                                    entityOnMap.Value.PlayerId == playerView.MyId)
+                                {
+                                    rightUpEval -= entityOnMap.Value.Health;
+                                }
+                            }
+                        }
+
+                        int rightDownEval = 0;
+                        for (int y = entity.Position.Y; y <= entity.Position.Y + 10 && y < 80; y++)
+                        {
+                            for (int x = entity.Position.X; x >= entity.Position.X - 10 && x >= 0; x--)
+                            {
+                                var entityOnMap = map[x, y];
+                                if (entityOnMap == null)
+                                {
+                                    continue;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.Resource)
+                                {
+                                    var distance = GetDistance(entity.Position, x, y);
+                                    rightDownEval += entityOnMap.Value.Health / distance;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.BuilderUnit && 
+                                    entityOnMap.Value.PlayerId == playerView.MyId)
+                                {
+                                    rightDownEval -= entityOnMap.Value.Health;
+                                }
+                            }
+                        }
+
+                        int leftDownEval = 0;
+                        for (int y = entity.Position.Y; y <= entity.Position.Y + 10 && y < 80; y++)
+                        {
+                            for (int x = entity.Position.X; x <= entity.Position.X + 10 && x < 80; x++)
+                            {
+                                var entityOnMap = map[x, y];
+                                if (entityOnMap == null)
+                                {
+                                    continue;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.Resource)
+                                {
+                                    var distance = GetDistance(entity.Position, x, y);
+                                    leftDownEval += entityOnMap.Value.Health / distance;
+                                }
+
+                                if (entityOnMap.Value.EntityType == EntityType.BuilderUnit &&
+                                    entityOnMap.Value.PlayerId == playerView.MyId)
+                                {
+                                    leftDownEval -= entityOnMap.Value.Health;
+                                }
+                            }
+                        }
+
+                        int leftEval = entity.Position.X + 1 < 80 && map[entity.Position.X + 1, entity.Position.Y] == null ? leftUpEval + leftDownEval : int.MinValue;
+                        int upEval = entity.Position.Y - 1 >= 0 && map[entity.Position.X, entity.Position.Y - 1] == null ? leftUpEval + rightUpEval : int.MinValue;
+                        int rightEval = entity.Position.X - 1 >= 0 && map[entity.Position.X - 1, entity.Position.Y] == null ? rightUpEval + rightDownEval : int.MinValue;
+                        int downEval = entity.Position.Y + 1 < 80 && map[entity.Position.X, entity.Position.Y + 1] == null ? rightDownEval + leftDownEval : int.MinValue;
+
+                        if (leftEval >= upEval &&
+                            leftEval >= rightEval &&
+                            leftEval >= downEval)
+                        {
+                            moveAction = new MoveAction(new Vec2Int(entity.Position.X + 1, entity.Position.Y), false,
+                                false);
+                        }
+                        else if (upEval >= leftEval &&
+                                 upEval >= rightEval &&
+                                 upEval >= downEval)
+                        {
+                            moveAction = new MoveAction(new Vec2Int(entity.Position.X, entity.Position.Y - 1), false,
+                                false);
+                        }
+                        else if (rightEval >= upEval &&
+                                 rightEval >= leftEval &&
+                                 rightEval >= downEval)
+                        {
+                            moveAction = new MoveAction(new Vec2Int(entity.Position.X - 1, entity.Position.Y), false,
+                                false);
+                        }
+                        else if (downEval >= upEval &&
+                                 downEval >= rightEval &&
+                                 downEval >= leftEval)
+                        {
+                            moveAction = new MoveAction(new Vec2Int(entity.Position.X, entity.Position.Y + 1), false,
+                                false);
+                        }
+
+                        EntityType[] resource = { EntityType.Resource };
+                        attackAction = new AttackAction(null, new AutoAttack(1, resource));
+                        entityActions.Add(entity.Id, new EntityAction(moveAction, null, attackAction, null));
+                        continue;
                     }
 
-                    EntityType[] builderTargets = { EntityType.Resource };
-                    attackAction = new AttackAction(null, new AutoAttack(playerView.MapSize * 2, builderTargets));
+                    case EntityType.MeleeBase:
+                        continue;
 
-                    entityActions.Add(entity.Id, new EntityAction(null, buildAction, attackAction, null));
-                    continue;
-                }
+                    case EntityType.MeleeUnit:
+                        continue;
 
-                if (entity.EntityType == EntityType.MeleeUnit || 
-                    entity.EntityType == EntityType.RangedUnit ||
-                    entity.EntityType == EntityType.Turret)
-                {
-                    EntityType[] unitTargets =
-                    {
-                        EntityType.MeleeUnit, 
-                        EntityType.RangedUnit, 
-                        EntityType.BuilderUnit, 
-                        EntityType.Turret
-                    };
-                    attackAction = new AttackAction(null, new AutoAttack(properties.SightRange, unitTargets));
-                }
-                else if (entity.EntityType == EntityType.MeleeBase)
-                {
-                    continue;
-                }
-                else if (entity.EntityType == EntityType.RangedBase)
-                {
-                    continue;
-                }
-                else if (properties.Build.HasValue)
-                {
-                    var entityType = properties.Build.Value.Options[0];
-                    var position = new Vec2Int(
-                        entity.Position.X,
-                        entity.Position.Y - 1
-                    );
+                    case EntityType.RangedBase:
+                        continue;
 
-                    buildAction = new BuildAction(entityType, position);
-                }
+                    case EntityType.RangedUnit:
+                        continue;
 
-                var entityAction = new EntityAction(moveAction, buildAction, attackAction, repairAction);
-                entityActions.Add(entity.Id, entityAction);
+                    case EntityType.Resource:
+                        continue;
+
+                    case EntityType.Turret:
+                        EntityType[] unitTargets =
+                        {
+                            EntityType.MeleeUnit,
+                            EntityType.RangedUnit,
+                            EntityType.BuilderUnit,
+                            EntityType.Turret
+                        };
+                        attackAction = new AttackAction(null, new AutoAttack(properties.SightRange, unitTargets));
+                        entityActions.Add(entity.Id, new EntityAction(null, null, attackAction, null));
+                        continue;
+                }
             }
 
             return new Action(entityActions);
         }
 
+        public int GetDistance(Vec2Int p1, int x, int y) => Math.Abs(p1.X - x) + Math.Abs(p1.Y - y);
+
         public void DebugUpdate(PlayerView playerView, DebugInterface debugInterface) 
         {
             debugInterface.Send(new DebugCommand.Clear());
             debugInterface.GetState();
-        }
-
-        public static double Distance(Vec2Int p1, Vec2Int p2)
-        {
-            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X)
-                             + (p1.Y - p2.Y) * (p1.Y - p2.Y));
-        }
-
-        public static bool IsInRect(Vec2Int p1, Vec2Int p2, int size)
-        {
-            if (p1.X >= p2.X &&
-                p1.Y >= p2.Y &&
-                p1.X <= p2.X + size &&
-                p1.Y <= p2.Y + size)
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
